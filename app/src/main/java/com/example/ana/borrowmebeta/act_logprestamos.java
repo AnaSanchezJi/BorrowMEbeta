59a111fa1a1711ab08be5ead87326391130d4a50
Ana Sanchez <anpasanchezji@ittepic.edu.mx>
2016-05-25 02:14:19 -0500

    Registro prestamos

--- app/src/main/java/com/example/ana/borrowmebeta/act_logprestamos.java
+++ app/src/main/java/com/example/ana/borrowmebeta/act_logprestamos.java
@@ -2,12 +2,23 @@ package com.example.ana.borrowmebeta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class act_logprestamos extends AppCompatActivity {
-
+    ImageView regresarlogpres;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act_logprestamos);
-    }
+        //Regresar Botton
+        regresarlogpres = (ImageView) findViewById(R.id.imageViewana1);
+        regresarlogpres.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               finish();
+            }
+        });
+
+    }//oncreate
 }
