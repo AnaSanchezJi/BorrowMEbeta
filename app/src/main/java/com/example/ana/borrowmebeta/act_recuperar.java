aba80dc40e7954192979223f68fa165ed348cfeb
ana <aniuska_pr@hotmail.com>
2016-05-25 16:57:52 -0600

    boton para salir

--- app/src/main/java/com/example/ana/borrowmebeta/act_recuperar.java
+++ app/src/main/java/com/example/ana/borrowmebeta/act_recuperar.java
@@ -15,8 +15,8 @@ public class act_recuperar extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act_recuperar);
         //Regresar Botton
-        regresarlogpres = (ImageView) findViewById(R.id.imageViewlucero1);
-        regresarlogpres.setOnClickListener(new View.OnClickListener() {
+        regresar= (ImageView) findViewById(R.id.imageViewlucero1);
+        regresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
