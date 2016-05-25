ceb6c63d6fccc7cc26c55b5acff0e248bc7aa149
AlbertoPedrozaCA <alb_ped_250@hotmail.com>
2016-05-24 23:25:11 -0500

    parcial programacion de botones pantalla principal

--- app/src/main/java/com/example/ana/borrowmebeta/MainActivity.java
+++ app/src/main/java/com/example/ana/borrowmebeta/MainActivity.java
@@ -2,12 +2,53 @@ package com.example.ana.borrowmebeta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView prestar,recuperar,verPS,verRec;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        //------------------------------
+        prestar= (ImageView) findViewById(R.id.imageView);
+        recuperar=(ImageView) findViewById(R.id.imageView3);
+        verPS=(ImageView) findViewById(R.id.imageView2);
+        verRec=(ImageView) findViewById(R.id.imageView4);
+        //--------------------------------
+
+        prestar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        //------------------------btnprestar
+        recuperar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        //-------------------------btn recuperar
+
+        verPS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        //-------------------------- log prestados
+        verRec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        //--------------------log recuperados
+
+    }// cierra on create
 }
