86b81453bdaa90a329ae569df33bccf925a0ff8b
ana <aniuska_pr@hotmail.com>
2016-05-25 18:16:29 -0600

    boton salir de articulos recuperados

--- app/src/main/java/com/example/ana/borrowmebeta/act_logrecuperados.java
+++ app/src/main/java/com/example/ana/borrowmebeta/act_logrecuperados.java
@@ -1,13 +1,25 @@
+
 package com.example.ana.borrowmebeta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class act_logrecuperados extends AppCompatActivity {
-
+    ImageView regresa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act_logrecuperados);
-    }
+        //Regresar Botton
+        regresa= (ImageView) findViewById(R.id.imageViewlucero3);
+        regresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }//oncreate
 }
